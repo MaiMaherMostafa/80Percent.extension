e9303665dcf1fd95a0067e86f8e51ffe41981b1c
MaiMaherMostafa <mai.maher.ce@gmail.com>
2026-04-05 18:09:32 +0200

    Delete

--- script.py
+++
@@ -1,9 +0,0 @@
-title: Columns
-tooltip: Automate the annotation needed for the columns and axes view (Columns and grids dimensions, tags and view template).
-author: Mai Maher
-engine:
-  clean: false
-  bin: python
-script: script.py
-icon: icon.png
-highlight: updated
